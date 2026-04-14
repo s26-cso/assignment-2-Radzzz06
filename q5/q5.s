4475fa040f5bf0e3a3e44db351a05ef3cfbc4cad
Radzzz06 <radhikasahu1975@gmail.com>
2026-04-14 22:59:39 +0530

    Update q5.s

--- q5/q5.s
+++ q5/q5.s
@@ -1,122 +1,100 @@
-    .equ SYS_OPENAT,  56
-    .equ SYS_CLOSE,   57
-    .equ SYS_LSEEK,   62
-    .equ SYS_READ,    63
-    .equ SYS_WRITE,   64
-    .equ SYS_EXIT,    93
-    .equ AT_FDCWD,   -100
-    .equ O_RDONLY,    0
-    .equ SEEK_SET,    0
-    .equ SEEK_END,    2
-    .equ STDOUT,      1
+.section .rodata
 
-    .section .data
+filename: .asciz  "input.txt"
+mode_r: .asciz  "r"
+yes_str: .asciz  "Yes\n"
+no_str: .asciz  "No\n"
 
+        .section .text
+        .globl main
 
-filename: .asciz "input.txt"
-msg_yes: .asciz "Yes\n"
-msg_no: .asciz "No\n"
+main:
+        addi sp, sp, -32
+        sw ra, 28(sp)   # Save return address
+        sw s1, 24(sp)   # s1 will hold the FILE pointer
+        sw s2, 20(sp)   # s2 will hold the file length
+        sw s3, 16(sp)   # s3 is the 'left' index
+        sw s4, 12(sp)   # s4 is the 'right' index
 
-    .section .text
-    .globl _start
+        # open the file
 
+        la a0, filename     # Load address of "input.txt"
+        la a1, mode_r   
+        call fopen
+        mv s1, a0   # Copy file pointer to s1
 
-#  s0 = file_left(file descriptor reading from the start)
-#  s1 = file_right(file descriptor reading from the end)
-#  s2 = left_pointer(current left byte offset)
-#  s3 = right_pointer(current right byte offset)
+        beqz s1, printNo    # If file pointer is 0 (NULL), jump to error
 
-_start:
-    addi    sp, sp, -8              # 1-byte scratch buffer on the stack
+        # seek to end to get file length
 
-    # open file_left
-    li      a7, SYS_OPENAT
-    li      a0, AT_FDCWD
-    la      a1, filename
-    li      a2, O_RDONLY
-    li      a3, 0
-    ecall
-    mv      s0, a0                  # s0 = file_left
+        mv a0, s1           # a0 = file pointer
+        li a1, 0            # offset = 0
+        li a2, 2
+        call fseek
 
-    # open file_right
-    li      a7, SYS_OPENAT
-    li      a0, AT_FDCWD
-    la      a1, filename
-    li      a2, O_RDONLY
-    li      a3, 0
-    ecall
-    mv      s1, a0                  # s1 = file_right
+        mv a0, s1
+        call ftell          # Get current cursor position (length)
+        mv s2, a0           # Store length in s2
 
-    # find file length using lseek(file_right, 0, SEEK_END)
-    li      a7, SYS_LSEEK
-    mv      a0, s1
-    li      a1, 0
-    li      a2, SEEK_END
-    ecall                           # a0 = file length
+        # set up left and right pointers
 
-    li      s2, 0                   # left_pointer  = 0
-    addi    s3, a0, -1              # right_pointer = length - 1
+        li s3, 0
+        addi s4, s2, -1     # Right = Length - 1
 
-    bltz    s3, printYes           # empty file,still palindrome
+        # 0 or 1 characters is always a palindrome
 
-loop:
-    bge     s2, s3, printYes       # pointers met,hence palindrome
+        blez s4, printYes
 
-    # read left character
-    li      a7, SYS_LSEEK
-    mv      a0, s0
-    mv      a1, s2
-    li      a2, SEEK_SET
-    ecall
-    li      a7, SYS_READ
-    mv      a0, s0
-    mv      a1, sp
-    li      a2, 1
-    ecall
-    lb      t0, 0(sp)               # t0 = left char
+check:
+        bge s3, s4, printYes
 
-    # read right character
-    li      a7, SYS_LSEEK
-    mv      a0, s1
-    mv      a1, s3
-    li      a2, SEEK_SET
-    ecall
-    li      a7, SYS_READ
-    mv      a0, s1
-    mv      a1, sp
-    li      a2, 1
-    ecall
-    lb      t1, 0(sp)               # t1 = right char
+        # read char at left
+        mv a0, s1       # File pointer
+        mv a1, s3
+        li a2, 0
+        call fseek      # SEEK_SET = 0
+        mv a0, s1
+        call fgetc      # Read one byte
+        mv t1, a0       # Store char in t1
 
-    bne     t0, t1, printNo        # mismatch hence not a palindrome
+        # read char at right
+        mv a0, s1
+        mv a1, s4       # Current right offset
+        li a2, 0        # SEEK_SET = 0
+        call fseek
+        mv a0, s1
+        call fgetc
+        mv t2, a0       # Store char in t2
 
-    addi    s2, s2, 1               # left_pointer++
-    addi    s3, s3, -1              # right_pointer--
-    j       loop
+        bne t1, t2, printNo     # If left char != right char, fail
+
+        addi s3, s3, 1
+        addi s4, s4, -1
+        j check
 
 printYes:
-    li      a7, SYS_WRITE
-    li      a0, STDOUT
-    la      a1, msg_yes
-    li      a2, 4
-    ecall
-    j       closure
+        mv a0, s1       # Close file before exiting
+        call fclose
+        la a0, yes_str
+        call printf
+        j done
 
 printNo:
-    li      a7, SYS_WRITE
-    li      a0, STDOUT
-    la      a1, msg_no
-    li      a2, 3
-    ecall
+        beqz s1, no_file_to_close     # Don't close if file never opened
+        mv a0, s1
+        call fclose
 
-closure:
-    li      a7, SYS_CLOSE
-    mv      a0, s0
-    ecall
-    li      a7, SYS_CLOSE
-    mv      a0, s1
-    ecall
-    addi    sp, sp, 8
-    li      a7, SYS_EXIT
-    li      a0, 0
-    ecall
+no_file_to_close:
+        la a0, no_str
+        call printf
+
+done:
+#empty the stack
+        lw ra, 28(sp)
+        lw s1, 24(sp)
+        lw s2, 20(sp)
+        lw s3, 16(sp)
+        lw s4, 12(sp)
+        addi sp, sp, 32
+        li a0, 0
+        ret
